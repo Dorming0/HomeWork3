b3b60b9d41369cb58bc924da2809ddc51c8573d9
Юрий <goker21@inbox.ru>
2024-01-30 16:13:10 +0300

    DopHomeWorkДобавьте файлы проекта.

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,165 @@
+﻿
+#region Задание 1
+for (int i = 0; i < 100; i++)
+{
+    if (i % 2 != 0)
+    {
+        Console.WriteLine(i);
+    }
+}
+#endregion
+
+#region Задание 2
+for (int i = 5; i > 0; i--)
+{
+    Console.WriteLine(i);
+}
+#endregion
+#region Задание 3
+Console.WriteLine("Введите число :");
+int value = Convert.ToInt32(Console.ReadLine());
+int valueResult = 0;
+for (int i = 1; i <= value; i++)
+{
+    valueResult += i;
+}
+Console.WriteLine(valueResult);
+
+
+#endregion
+#region Задание 4
+
+
+int a = 0;
+
+while (a < 100)
+{
+    a++;
+    if (a % 7 == 0)
+    {
+        Console.WriteLine(a);
+    }
+}
+
+#endregion
+#region Задание 5
+int[] array1 = new int[5];
+int[] array2 = new int[5];
+
+FillArray(array1);
+FillArray(array2);
+
+CalculateArithmeticaMean(array1, array2);
+
+//int sum = 0;
+//Console.WriteLine("Введите данные массива №1 через 'пробел'");
+//string[] arrayNumber = Console.ReadLine().Split(' ');
+
+//Console.WriteLine("Введите данные массива №2 через 'пробел'");
+//string[]arrayNumber2 = Console.ReadLine().Split(' ');
+
+//for (int i = 0; i < arrayNumber.Length; i++)
+//{
+//    array1[i] = int.Parse(arrayNumber[i]);
+//}
+//for (int i = 0;i < arrayNumber2.Length; i++)
+//{
+//    array2[i] = int.Parse(arrayNumber2[i]);
+//}
+
+//for(int i = 0;i< array1.Length; i++)
+//{
+//    sum += array1[i];
+//}
+//int result = sum / array1.Length;
+
+//sum = 0;
+
+//for (int j = 0; j < array2.Length; j++)
+//{
+
+//    sum += array2[j];
+//}
+//int result2 = sum / array2.Length;
+
+
+//if (result > result2)
+//{
+//    Console.WriteLine($"Средне арифметическое массива №1 равно {result}");
+//}
+//else if (result < result2)
+//{
+//    Console.WriteLine($"Средне арифметическое массива №2 равно {result2}");
+//}
+//else if (result == result2)
+//{
+//    Console.WriteLine($"Средне арифметическое массивов равны");
+//}
+
+static void FillArray( int[] array)
+{
+    Console.WriteLine("Введите данные массива ");
+
+    int filledNumbers = 0;
+
+    while (true)
+    {
+        if (filledNumbers == 5)
+        {
+            break;
+        }
+
+        var numberStr = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(numberStr) || string.IsNullOrWhiteSpace(numberStr))
+        {
+            Console.WriteLine("Введите число");
+
+            continue;
+        }
+
+        if (!int.TryParse(numberStr, out var number))
+        {
+            Console.WriteLine("Введите числовое значение");
+
+            continue;
+        }
+
+        array[filledNumbers] = number;
+
+        filledNumbers++;
+    }
+}
+
+static void CalculateArithmeticaMean(int[] array1, int[] array2)
+{
+    var firstSum = 0;
+    var twiceSum = 0;
+
+    for (int i = 0; i < array1.Length; i++)
+        firstSum += array1[i];
+
+    firstSum /= array1.Length;
+
+    for (int j = 0; j < array2.Length; j++)
+        twiceSum += array2[j];
+
+    twiceSum /= array2.Length;
+
+    if (firstSum > twiceSum)
+    {
+        Console.WriteLine($"Средне арифметическое массива №1 равно {firstSum}");
+    }
+    else if (firstSum < twiceSum)
+    {
+        Console.WriteLine($"Средне арифметическое массива №2 равно {twiceSum}");
+    }
+    else if (firstSum == twiceSum)
+    {
+        Console.WriteLine($"Средне арифметическое массивов равны");
+    }
+
+
+}
+
+#endregion
---
+++ HomeWorkDop/Program.cs
@@ -0,0 +1,111 @@
+﻿Console.Write("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine());
+Console.WriteLine();
+int[,] myArray = new int[size, size];
+Random rank = new Random();
+for (int i = 0; i < myArray.GetLength(0); i++)
+{
+    for (int j = 0; j < myArray.GetLength(1); j++)
+    {
+        myArray[i, j] = rank.Next(-10, 10);
+        Console.Write(myArray[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Какое действие выполнить : " +
+    "\n 1 - Найти количество положительных/отрицательных чисел в матрице\r" +
+    "\n 2 - Сортировка элементов матрицы построчно (в двух направлениях)\r" +
+    "\n 3 - Инверсия элементов матрицы построчно");
+int action = int.Parse(Console.ReadLine());
+switch (action)
+{
+    case 1:
+        NumericalValue(myArray);
+        break;
+
+    case 2:
+        Console.WriteLine("Какую сортировку выполнить?: " +
+        "\n 1 - Положительную\r " +
+        "\n 2 - Отрицательную\r ");
+        int sortArray = Convert.ToInt32(Console.ReadLine());
+        SortingItems(myArray, sortArray);
+        break;
+
+    case 3:
+        Inversion(myArray);
+        break;
+}
+static void NumericalValue(int[,] array)
+{
+    int countPositive = 0;
+    int countNegative = 0;
+    int zero = 0;
+    foreach (int item in array)
+    {
+        if (item > 0)
+            countPositive++;
+
+        else if (item < 0)
+            countNegative++;
+        else if (item == 0)
+            zero++;
+    }
+
+    Console.WriteLine($"Количество положительных чисел в массиве {countPositive}");
+    Console.WriteLine($"Количество отрицательных чисел в массиве {countNegative}");
+    Console.WriteLine($"Количество нолей в массиве {zero}");
+}
+static void SortingItems(int[,] array, int sort)
+{
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int c = j + 1; c < array.GetLength(1); c++)
+            {
+                if (sort == 1)
+                {
+                    if (array[i, j] > array[i, c])
+                    {
+                        result = array[i, j];
+                        array[i, j] = array[i, c];
+                        array[i, c] = result;
+                    }
+                }
+                else
+                {
+                    if (array[i, j] < array[i, c])
+                    {
+                        result = array[i, j];
+                        array[i, j] = array[i, c];
+                        array[i, c] = result;
+
+                    }
+                }
+            }
+        }
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+static void Inversion(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = array.GetLength(1) - 1; j >= 0; j--)
+        {
+
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
